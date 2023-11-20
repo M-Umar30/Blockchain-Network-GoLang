833e0efd2bb3f5733de07b32001ef076b99e3948
M-Umar30 <muhammadumar2248@gmail.com>
2023-11-21 01:01:12 +0500

    Added Block & Hash functionality

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 /*
 	20I-0518 - Muhammad Umar
-	20I-
+	20I-0450 - Fatima Zubeda
 	20L-
 	20I-
 */
 
-// Assignment 1: Block Chain Implementation
-
-// TODO: Seperate files for all hash related functions? 
-// TODO: Calculate hash of block
-// TODO: Complete merkel tree implementation
-// TODO: Block mining
-// TODO: Block validation
-// TODO: Chain validation
-
-// Assignment 2: P2P Implementation
-
-
-package main 
+package main
 
 import (
 	"fmt"
+	"encoding/hex"
 )
 
+// TODO: Verifying block and chain (shouldn't it be like a given that its verified cuz implementation?) just make a function which iterates through everything maybe?
 func main() {
-	fmt.Println("Hello World!")
+	// Example data
+	prevBlockHash, _ := hex.DecodeString("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef")
+	merkleRoot, _ := hex.DecodeString("abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789")
+
+	// Create a block
+	block := create_block([32]byte(prevBlockHash), [32]byte{}, []string{"Transaction1", "Transaction2"}, [32]byte(merkleRoot))
+
+	// Mine the block
+	success := block.mine()
+
+	if success {
+		fmt.Println("Block mined successfully!")
+	} else {
+		fmt.Println("Mining failed.")
+	}
 }
